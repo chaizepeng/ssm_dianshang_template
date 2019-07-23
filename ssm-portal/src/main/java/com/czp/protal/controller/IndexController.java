19f2897c66693aaf5543f0027dd92b07563f1218
chaizepeng <1397988895@qq.com>
2019-07-23 20:49:37 +0800

    httpclient测试功能实现

--- ssm-portal/src/main/java/com/czp/protal/controller/IndexController.java
+++ ssm-portal/src/main/java/com/czp/protal/controller/IndexController.java
@@ -2,6 +2,10 @@ package com.czp.protal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.czp.common.pojo.AjaxReturnResult;
 
 @Controller
 public class IndexController {
@@ -10,5 +14,11 @@ public class IndexController {
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping(value="/httpclient/post", method=RequestMethod.POST)
+	@ResponseBody
+	public String testPost(String name,String password) {
+		return "name:"+name+",password:"+password;
 	}
 }
--- ssm-portal/src/test/java/com/czp/httpclient/HttpClientTest.java
+++ ssm-portal/src/test/java/com/czp/httpclient/HttpClientTest.java
@@ -1,14 +1,20 @@
 package com.czp.httpclient;
 
-import java.net.URI;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.client.HttpClient;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
@@ -37,25 +43,60 @@ public class HttpClientTest {
 	
 	
 	@Test
-	public void doGetWithParam() throws Exception{
+	public void doGetWithParam() throws Exception {
 		// 创建httpclient对象
 		CloseableHttpClient httpClient = HttpClients.createDefault();
-		//URI uri;
+		// URI uri;
 		URIBuilder uriBuilder = new URIBuilder("http://www.sogou.com/web");
 		uriBuilder.addParameter("query", "花千骨");
-	// 创建get对象
-	HttpGet get = new HttpGet(uriBuilder.build());
-	// 执行请求
-			CloseableHttpResponse response = httpClient.execute(get);
-			// 取响应的结果
-			int statusCode = response.getStatusLine().getStatusCode();
-			System.out.println(statusCode);
-			HttpEntity entity = response.getEntity();
-			String string = EntityUtils.toString(entity,"utf-8");
-			System.out.println("读取的内容--->"+string);
-				
-				// 关闭httpclient
-				response.close();
-				httpClient.close();
+		// 创建get对象
+		HttpGet get = new HttpGet(uriBuilder.build());
+		// 执行请求
+		CloseableHttpResponse response = httpClient.execute(get);
+		// 取响应的结果
+		int statusCode = response.getStatusLine().getStatusCode();
+		System.out.println(statusCode);
+		HttpEntity entity = response.getEntity();
+		String string = EntityUtils.toString(entity, "utf-8");
+		System.out.println("读取的内容--->" + string);
+
+		// 关闭httpclient
+		response.close();
+		httpClient.close();
+	}
+
+	@Test
+	public void doPost() throws Exception{
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		// 创建post对象
+		HttpPost post = new HttpPost("http://localhost:8083/httpclient/post.html");
+		//执行post请求
+		CloseableHttpResponse response = httpClient.execute(post);
+		String result = EntityUtils.toString(response.getEntity());
+		System.out.println(result);
+		response.close();
+		httpClient.close();
+	}
+	
+	
+	@Test
+	public void doPostWithParam() throws Exception {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		// 创建post对象
+		HttpPost post = new HttpPost("http://localhost:8083/httpclient/post.html");
+		List<NameValuePair> kvList = new ArrayList<NameValuePair>();
+		kvList.add(new BasicNameValuePair("name", "小柴"));
+		kvList.add(new BasicNameValuePair("password", "123456"));
+
+		// 包装成一个Entity对象
+		StringEntity entity = new UrlEncodedFormEntity(kvList, "UTF-8");
+		// 设置请求内容
+		post.setEntity(entity);
+		// 执行post请求
+		CloseableHttpResponse response = httpClient.execute(post);
+		String result = EntityUtils.toString(response.getEntity());
+		System.out.println(result);
+		response.close();
+		httpClient.close();
 	}
 }
